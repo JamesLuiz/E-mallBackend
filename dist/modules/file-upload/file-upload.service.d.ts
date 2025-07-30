040cd3a147de675e405e201919658ce5d7f49f65
Eliezer James <101040559+JamesLuiz@users.noreply.github.com>
2025-07-30 22:29:35 +0100

    Deleted app.service.d.ts

--- dist/modules/file-upload/file-upload.service.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class FileUploadService {
-    private nftStorage;
-    constructor();
-    uploadFile(file: Express.Multer.File): Promise<string>;
-    uploadMultipleFiles(files: Express.Multer.File[]): Promise<string[]>;
-    deleteFile(cid: string): Promise<boolean>;
-}
