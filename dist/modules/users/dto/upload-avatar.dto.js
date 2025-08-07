19efa7aa9cff1928e7902c268c74c73c43abbfd7
Eliezer James <101040559+JamesLuiz@users.noreply.github.com>
2025-08-07 09:23:58 +0100

    Deleted app.service.d.ts

--- dist/modules/users/dto/upload-avatar.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UploadAvatarDto = void 0;
-class UploadAvatarDto {
-}
-exports.UploadAvatarDto = UploadAvatarDto;
-//# sourceMappingURL=upload-avatar.dto.js.map
