67f9c08cdfd113efec2525eb207e3d592b1c71a4
Eliezer James <101040559+JamesLuiz@users.noreply.github.com>
2025-09-13 00:51:47 +0100

    Added business-category.enum.js.map

---
+++ dist/common/enums/business-category.enum.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BusinessCategory = void 0;
+var BusinessCategory;
+(function (BusinessCategory) {
+    BusinessCategory["ELECTRONICS"] = "electronics";
+    BusinessCategory["FASHION_AND_STYLE"] = "fashion_and_style";
+    BusinessCategory["HOME_AND_GARDEN"] = "home_and_garden";
+    BusinessCategory["BEAUTY_AND_HEALTH"] = "beauty_and_health";
+    BusinessCategory["SPORTS_AND_FITNESS"] = "sports_and_fitness";
+    BusinessCategory["BOOKS_AND_MEDIA"] = "books_and_media";
+})(BusinessCategory || (exports.BusinessCategory = BusinessCategory = {}));
+//# sourceMappingURL=business-category.enum.js.map
